938966622d7e9e1b3c1859b34eda8f7b78917c53
Qi <93983104+QiGreenwhich@users.noreply.github.com>
2022-02-12 11:24:40 +0800

    fix dug

--- js/loaddata.js
+++ js/loaddata.js
@@ -12,30 +12,4 @@ function loaddata() {
         }
     }
 }
-function show()
-{
-  var box = document.getElementById("divcontent");
-  var text = box.innerHTML;
-  var newBox = document.createElement("div");
-  var btn = document.createElement("a");
-  btn.setAttribute("class","btn");
-  newBox.innerHTML = text.substring(0,66);
-  btn.innerHTML = text.length > 66 ? "显示全部 ▼" : "";
-  btn.href = "javascript:void(0)";
-  btn.onclick = function(){
-    if(btn.innerHTML == "显示全部 ▼")
-    {
-      btn.innerHTML = "收起 ▲";
-      newBox.innerHTML = text;
-    }
-    else
-    {
-      btn.innerHTML = "显示全部 ▼";
-      newBox.innerHTML = text.substring(0,66);
-    }
-  }
-  box.innerHTML = "";
-  box.appendChild(newBox);
-  box.appendChild(btn);
-}
 
